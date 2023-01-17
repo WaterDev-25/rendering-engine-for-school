d842d34c76b921305c6bfab7f4eaadd91574e844
waterdev <67019702+WaterDev-25@users.noreply.github.com>
2023-01-17 20:01:57 +0100

    exploded view, shader modification for multiple lights, add new gif to resources

--- engine/main.cpp
+++ engine/main.cpp
@@ -17,7 +17,15 @@ glm::mat4 GetViewMatrix()
     float camX = static_cast<float>(sin((SDL_GetTicks() / 1000.0f)) * radius);
     float camZ = static_cast<float>(cos((SDL_GetTicks() / 1000.0f)) * radius);
 
-    return glm::lookAt(glm::vec3(camX, 3.0f, camZ), glm::vec3(-2.0f, 0.0f, -2.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    //return glm::lookAt(glm::vec3(camX, 3.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+    double currentTime = SDL_GetTicks() / 1000.0f;
+    double angle = currentTime * 1.0f;
+
+    glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 10.0f) + glm::vec3(camX, 3.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 rotatedView = glm::rotate(view, (float)angle, glm::vec3(1.0f, 0.0f, 0.0f));
+
+    return rotatedView;
 }
 
 int main(int argc, char** argv)
@@ -36,7 +44,9 @@ int main(int argc, char** argv)
     CShader baseShader("shaders/base_vs.glsl", "shaders/base_fs.glsl");
     CShader cubeMapShader("shaders/cubemap_vs.glsl", "shaders/cubemap_fs.glsl");
 
-    CModel baseModel("models/nfc/asm.obj");
+    //CModel baseModel("models/nfc/asm.obj");
+    CModel partOne("models/nfc/part1.obj");
+    CModel partTwo("models/nfc/part2.obj");
 
     std::vector<std::string> cubeMapTextures = {
         "textures/cubemap/maskonaive/right.jpg",
@@ -76,30 +86,46 @@ int main(int argc, char** argv)
 
         baseShader.Use();
 
-        baseShader.SetVec3("light.position", 0.5f, -1.0f, -1.0f);
         baseShader.SetVec3("viewPos", GetViewMatrix()[3]);
-
-        baseShader.SetVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-        baseShader.SetVec3("light.diffuse", 1.0f, 1.0f, 1.0f);
-        baseShader.SetVec3("light.specular", 1.0f, 1.0f, 1.0f);
-        baseShader.SetFloat("light.constant", 1.0f);
-        baseShader.SetFloat("light.linear", 0.09f);
-        baseShader.SetFloat("light.quadratic", 0.032f);
-
         baseShader.SetFloat("material.shininess", 32.0f);
+
+        baseShader.SetVec3("pointLights[0].position", 0.0f, -1.0f, 0.0f);
+        baseShader.SetVec3("pointLights[0].ambient", 0.2f, 0.2f, 0.2f);
+        baseShader.SetVec3("pointLights[0].diffuse", 1.0f, 1.0f, 1.0f);
+        baseShader.SetVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+        baseShader.SetFloat("pointLights[0].constant", 1.0f);
+        baseShader.SetFloat("pointLights[0].linear", 0.09f);
+        baseShader.SetFloat("pointLights[0].quadratic", 0.032f);
+
+        baseShader.SetVec3("pointLights[1].position", 0.0f, 2.0f, 0.0f);
+        baseShader.SetVec3("pointLights[1].ambient", 0.2f, 0.2f, 0.2f);
+        baseShader.SetVec3("pointLights[1].diffuse", 1.0f, 1.0f, 1.0f);
+        baseShader.SetVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
+        baseShader.SetFloat("pointLights[1].constant", 1.0f);
+        baseShader.SetFloat("pointLights[1].linear", 0.09f);
+        baseShader.SetFloat("pointLights[1].quadratic", 0.032f);
 
         glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 view = GetViewMatrix();
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)wWidth / (float)wHeight, 0.1f, 100.0f);
         model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
         model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
-        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        //model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 
         baseShader.SetMat4("model", model);
         baseShader.SetMat4("projection", projection);
         baseShader.SetMat4("view", view);
         
-        baseModel.Draw(baseShader);
+        //baseModel.Draw(baseShader);
+        partOne.Draw(baseShader);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+
+        baseShader.SetMat4("model", model);
+
+        partTwo.Draw(baseShader);
 
         cubeMapShader.Use();
         view = glm::mat4(glm::mat3(GetViewMatrix()));
